830edd70f16e167cb616844740329513a8c093b9
Eugen Tudorancea <eugen.tudorancea@gmail.com>
2018-05-26 00:46:02 +0300

    Add public folder to source, and publish to gh-pages

--- playground.js
+++
@@ -1,6 +0,0 @@
-import getConfig from "next/config";
-
-const { publicRuntimeConfig } = getConfig();
-
-export const URL = publicRuntimeConfig.publicURL;
-export const STATIC = publicRuntimeConfig.staticURL;
