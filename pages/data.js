eccc0e6200347bf156d673cb4574c17c81c595c0
Elena Pavel <t.twixx7@gmail.com>
2018-05-28 05:57:21 +0300

    Fixes on paths.

--- pages/data.js
+++ pages/data.js
@@ -5,32 +5,32 @@ export default {
 		{
 			image: STATIC + "/screenshots/architecture.png",
 			title: "Architecture",
-			link: URL + "/architecture",
+			link: "/architecture",
 		},
 		{
 			image: STATIC + "/screenshots/plants.png",
 			title: "Plants",
-			link: URL + "/plants",
+			link: "/plants",
 		},
 		{
 			image: STATIC + "/screenshots/books.png",
 			title: "Books",
-			link: URL + "/books",
+			link: "/books",
 		},
 		{
 			image: STATIC + "/screenshots/universe-facts.png",
 			title: "Universe Facts",
-			link: URL + "/universe-facts",
+			link: "/universe-facts",
 		},
 		{
 			image: STATIC + "/screenshots/calendar.png",
 			title: "Calendar",
-			link: URL + "/calendar",
+			link: "/calendar",
 		},
 		{
 			image: STATIC + "/screenshots/react-wireframe.png",
 			title: "React Wireframe",
-			link: URL + "/react-wireframe",
+			link: "/react-wireframe",
 		},
 	],
 };
