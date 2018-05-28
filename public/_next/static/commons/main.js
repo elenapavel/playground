ba327bc3e0ca6394bcbe7b09cf34620dc7398f6d
Elena Pavel <t.twixx7@gmail.com>
2018-05-28 05:06:01 +0300

    Compiled files.

--- public/_next/static/commons/main.js
+++ public/_next/static/commons/main.js
@@ -36029,7 +36029,41 @@ module.exports = {"application":"application_1E6rR","content":"content_-hwSI","m
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1527459647971");
+            injectCss(link, link.href.split("?")[0] + "?unix=1527471098847");
+          });
+      }
+    }
+  
+
+/***/ }),
+
+/***/ "./pages/style.css":
+/***/ (function(module, exports, __webpack_require__) {
+
+// removed by extract-text-webpack-plugin
+module.exports = {"application":"application_2w-7Z","container":"container_37FNP","content":"content_3kBAR","separator":"separator_Y6LAk","title":"title_2QJ8l","description":"description_O8mfF","items":"items_geZBh","item":"item_2DZR7","item_image":"item_image_hY6-A","item_link":"item_link_dpW1G","item_link_hovered":"item_link_hovered_2-Z3I item_link_dpW1G","arrow":"arrow_3FPn8"};;
+    if (true) {
+      var injectCss = function injectCss(prev, href) {
+        var link = prev.cloneNode();
+        link.href = href;
+        link.onload = function() {
+          prev.parentNode.removeChild(prev);
+        };
+        prev.stale = true;
+        prev.parentNode.insertBefore(link, prev);
+      };
+      module.hot.dispose(function() {
+        window.__webpack_reload_css__ = true;
+      });
+      if (window.__webpack_reload_css__) {
+        module.hot.__webpack_reload_css__ = false;
+        console.log("[HMR] Reloading stylesheets...");
+        var prefix = document.location.protocol + '//' + document.location.host;
+        document
+          .querySelectorAll("link[href][rel=stylesheet]")
+          .forEach(function(link) {
+            if (!link.href.match(prefix) || link.stale) return;
+            injectCss(link, link.href.split("?")[0] + "?unix=1527473075935");
           });
       }
     }
