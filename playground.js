c5c5e04abec03f5d92a2799aa8a57f6bf710cd86
Eugen Tudorancea <eugen.tudorancea@gmail.com>
2018-05-29 02:29:42 +0300

    Re-export public

--- playground.js
+++
@@ -1,7 +0,0 @@
-import getConfig from "next/config";
-
-const { publicRuntimeConfig } = getConfig();
-
-export const BASE = publicRuntimeConfig.base;
-export const URL = publicRuntimeConfig.url;
-export const STATIC = publicRuntimeConfig.static;
